0ea991319230def5587fbd27113aa175b1074d6e
7akase <yasuhide.takase@gmail.com>
2016-06-06 10:05:26 +0900

    test CR decay response

--- taste_ode.py
+++ taste_ode.py
@@ -1,20 +1,19 @@
-# from numpy import *
+from numpy import *
 from scipy.integrate import *
 
-y0, t0 = [1.0, 2.0], 0
+y0, t0 = [1.0, 1.0], 0
 
-def f(t, y, arg1):
-        return [arg1*y[0] + y[1], -arg1*y[1]**2]
-
-def jac(t, y, arg1):
-        return [[arg1, 1], [0, -arg1*2*y[1]]]
+def f(t, y, trf, tau):
+        # Y/X = 1/(1+s*tau)
+        # <=> Y + s*tau*Y = X <=> sY = (X-Y)/tau
+        return [(0-y[0])/trf, (0 - y[1])/tau]
 
 r = ode(f).set_integrator('dorpi5', with_jacobian=False)
 
-r.set_initial_value(y0, t0).set_f_params(2.0)
+r.set_initial_value(y0, t0).set_f_params(0.1, 1.0)
 
 t1 = 10
 dt = 1
 while r.successful() and r.t < t1:
         r.integrate(r.t+dt)
-        print("%g %g" %(r.t, abs(r.y[0])))
+        print("%g %g %g" %(r.t, r.y[0], r.y[1]))
