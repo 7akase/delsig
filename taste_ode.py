d42f50d4089b58130df68b8db1331777d23b510b
7akase <yasuhide.takase@gmail.com>
2016-06-06 09:56:08 +0900

    test ode with dorpi5 method

--- taste_ode.py
+++ taste_ode.py
@@ -1,16 +1,17 @@
 # from numpy import *
 from scipy.integrate import *
 
-y0, t0 = [1.0j, 2.0], 0
+y0, t0 = [1.0, 2.0], 0
 
 def f(t, y, arg1):
-        return [1j*arg1*y[0] + y[1], -arg1*y[1]**2]
+        return [arg1*y[0] + y[1], -arg1*y[1]**2]
 
 def jac(t, y, arg1):
-        return [[1j*arg1, 1], [0, -arg1*2*y[1]]]
+        return [[arg1, 1], [0, -arg1*2*y[1]]]
 
-r = ode(f).set_integrator('zvode', method='bdf', with_jacobian=False)
-r.set_initial_value(y0, t0).set_f_params(2.0).set_jac_params(2.0)
+r = ode(f).set_integrator('dorpi5', with_jacobian=False)
+
+r.set_initial_value(y0, t0).set_f_params(2.0)
 
 t1 = 10
 dt = 1
