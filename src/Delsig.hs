8c6cab7b4936a02b3a671dab09f6a2b08c56d385
7akase <yasuhide.takase@gmail.com>
2016-10-11 19:01:24 +0900

    clean up

--- src/Delsig.hs
+++ src/Delsig.hs
@@ -1,47 +1,75 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Delsig where
 
-import Control.Applicative
-import Text.Printf
 import Data.List
-import Data.Ix
-import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data
-import Psd
+import Numeric.GSL.ODE
 
-type VecEle = Vector Double
+import Control.Arrow
+import Control.Lens
 
--- view :: String -> [VecEle] -> IO ()
-view fmt = putStrLn . unlines . fmap (concat . (fmap (printf fmt)))
--- |
--- >>> view "%02d " [[0,1],[2,3]]
--- 00 01 
--- 02 03
-calculateSNR :: [Double] -> Int -> Int -> Double
-calculateSNR hwfft f nsig =
-  dbp $ s / n
-    where
-      len = length hwfft
-      signalBins = filter (inRange (0,len)) [f-nsig+1..f+nsig]
-      s = sum $ fmap (hwfft !!) signalBins
-      noiseBins  = [0..len-1] \\ signalBins
-      n = norm $ fmap (hwfft !!) noiseBins
-      
-      
-hann :: Int -> [Double]
-hann n = (0.5 *) . (1.0 -) . cos . (2*pi*) <$> xs
+-- | # Dimensinos 
+type Time       = Double
+type Freq       = Double
+type AngularFreq = Double
+
+type Voltage    = Double
+type Current    = Double
+
+type SignalSource  = Time -> Voltage
+
+type Osr       = Double
+type Bit       = Int
+
+-- | # Modulator
+data Modulator = Modulator {
+                   _fs :: Freq,
+                   _osr :: Osr,
+                   _n_dac :: Bit
+                 } deriving (Show)
+$(makeLenses ''Modulator)
+
+-- data StateVector = StateVector{
+
+m = Modulator{ _fs = 1e0, _osr = 256, _n_dac = 8}
+
+-- | # Simulator
+
+run :: SignalSource -> (Time, [Voltage]) -> [Time] -> [(Time, [Voltage])]
+
+run :: (Time, [Voltage]) -> [Time] -> [(Time, [Voltage])]
+run = scanl (flip runOneTimeSlot)
+
+runOneTimeSlot :: Time -> (Time, [Voltage]) -> (Time, [Voltage])
+-- | 
+--
+runOneTimeSlot dt = (id *** runDt) . runCt dt
+
+runDt :: [Voltage] -> [Voltage]
+runDt [u, zv, zzv, zfb, x, y] = [u, v, zv, fb, x', y]
   where
-    n' = fromIntegral n :: Double
-    xs = (/ (n' - 1)) <$> [0 .. n' - 1]
+    v     = if y >= 0 then 1 else -1
+    fb    = zfb + (2 * v - zv) / 2**n_dac
+    x'    = (u - fb) / 2
+    n_dac = 8
+
+runCt :: Time -> (Time, [Voltage]) -> (Time, [Voltage])
+runCt dt (t, vs) = (t', vs')
+  where 
+    t' = t + dt
+    vs' = last . toLists . odeSolve xdot vs $ linspace 2 (t, t')
+    xdot t [u, v, zv, fb, x, y] = [su, sv, szv, sfb, sx, sy]
+      where
+        su  = f' t
+        sv  = 0
+        szv = 0
+        sfb = 0
+        sx  = (su - sfb) / 2
+        sy  = wp0 * (x - y + sx / wz0)
+        wp0 = 2 * pi * 1e0 / 100 :: AngularFreq
+        wz0 = 2 * pi * 1e0 / 2 :: AngularFreq
 
 
--- support funciton
-norm :: [Double] -> Double
-norm = sum . fmap (^ 2)
-
-dbv :: Double -> Double
-dbv x = 20 * log x / log 10
-
-dbp :: Double -> Double
-dbp x = 10 * log x / log 10
-
-  
+-- f t = 1.0 / 256 * (_osr
+f' :: Time -> Voltage
+f' = id 
